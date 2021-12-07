9285e697c3b057e48107db59bc839fd930c60c8c
jankd90 <j.kleinedeters.90@gmail.com>
2021-12-07 10:12:01 +0100

    updated

--- ble_1.py
+++ ble_1.py
@@ -45,23 +45,7 @@ def setup_notifications(p):
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self,params):
         btle.DefaultDelegate.__init__(self)
-    def write_to_db(self, adr, value):
-        timestamp = int(time.time()*1000000000)
-        #print("Address: "+adr)
-        json_body = [
-            {
-            "measurement": adr,
-            "tags": {
-            "host": "server01",
-            "region": "assen"
-            },
-            "time": timestamp,
-            "fields": {
-            "value": value
-            }
-            }
-            ]
-        client.write_points(json_body)
+    
     def handleNotification(self,cHandle,data):
         if cHandle == 21 or cHandle == 24:
             adr = "{0}:{1}".format(addr,cHandle-1) 
@@ -79,22 +63,99 @@ class MyDelegate(btle.DefaultDelegate):
 ##        print(cHandle)
         #print(value)
 
+def write_to_db(adr, value):
+        timestamp = int(time.time()*1000000000)
+        #print("Address: "+adr)
+        json_body = [
+            {
+            "measurement": adr,
+            "tags": {
+            "host": "server01",
+            "region": "assen"
+            },
+            "time": timestamp,
+            "fields": {
+            "value": value
+            }
+            }
+            ]
+        client.write_points(json_body)
+
 
 p = btle.Peripheral(addr)
 p_delegate = MyDelegate(addr)
 p.withDelegate(p_delegate)
-setup_notifications(p)
+#setup_notifications(p)
+chars = p.getCharacteristics()
+for c in chars:
+    print(c)
+    if(c.uuid == char_uuid):
+        print(c.read())
 print("connected")
 
+def read_values():
+    for c in chars:
+        #print("cool")
+        if(c.uuid == char_uuid ):  
+            data = c.read()
+            #print("reading...", data)
+            adr = "{0}:{1}".format(addr,10) 
+            value = int.from_bytes(data, byteorder=sys.byteorder)
+            #print("reading...", value)
+            write_to_db(adr, value)
+        if(c.uuid == char_uuid2 ):
+            data = c.read()
+            adr = "{0}:{1}".format(addr,11) 
+            value = int.from_bytes(data, byteorder=sys.byteorder)
+            write_to_db(adr, value)
+        if(c.uuid == char_uuid3 ):
+            data = c.read()
+            adr = "{0}:{1}".format(addr,12) 
+            value = int.from_bytes(data, byteorder=sys.byteorder)
+            write_to_db(adr, value)
+        if(c.uuid == char_uuid4 ):
+            data = c.read()
+            #print(data[0])
+            adr = "{0}:{1}".format(addr,13) 
+            value = int.from_bytes(data[:2], byteorder=sys.byteorder)
+            write_to_db(adr, value)
+            adr = "{0}:{1}".format(addr,14) 
+            value = int.from_bytes(data[2:], byteorder=sys.byteorder)
+            write_to_db(adr, value)
+        if(c.uuid == char_uuid5 ):
+            data = c.read()
+            #print(data[0])
+            adr = "{0}:{1}".format(addr,15) 
+            value = int.from_bytes(data[:2], byteorder=sys.byteorder)
+            write_to_db(adr, value)
+            adr = "{0}:{1}".format(addr,16) 
+            value = int.from_bytes(data[2:], byteorder=sys.byteorder)
+            write_to_db(adr, value)
+        if(c.uuid == char_uuid6 ):
+            data = c.read()
+            adr = "{0}:{1}".format(addr,17) 
+            value = int.from_bytes(data, byteorder=sys.byteorder)
+            write_to_db(adr, value)
+            
+    
+
 def reestablish_connection():
-    p.connect(addr)
-    print("reconnected")
-    setup_notifications(p)
+   while True:
+        try:
+            p.connect(addr)
+            print("reconnected")
+            return
+        except:
+            print("couldn't reconnect")
+            continue
+   
+  
 
 while True:
     try:
-        if p.waitForNotifications(1.0):
-            continue
+        #print("try")
+        read_values()
+        #time.sleep(1.0)
     except:
         try:
             p.disconnect()
